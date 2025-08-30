36195a6306e9783224132d5619f25838fef0ec89
danielfioresee <fiorese.silva.vitor@escola.pr.gov.br>
2025-08-29 21:18:18 -0300

    danier

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-<script>
-function inserirNome(){
- let nomeUsuario = prompt("Qual o seu nome");
- let elemento = document.querySelector("#nome-usuario");
- elemento.textContent = nomeUsuario;
-}
-inserirNome();
-</script>
+// Função que exibe um alerta com uma dica de moda
+document.getElementById('estilo-btn').addEventListener('click', function() {
+    alert('Dica de estilo: Invista em peças-chave que combinem com seu estilo pessoal, como acessórios únicos ou calçados confortáveis!');
+});
+
